5d11e34cfda39e6574db6d0ba3faf76daaa19cae
Jon Jarboe <jon.jarboe@sencha.com>
2016-04-12 16:05:31 -0500

    add prebuild script

---
+++ prebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to prepare the project in "Workspace" directory to be built
+# This will be used to simplify running tests in a continuous integration
+# environment under Jenkins
+
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $BASEDIR/Workspace
+python update-ext.py 6.0.3.21 ext
